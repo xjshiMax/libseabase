4586b2725074c0854b67623293302865e5c43583
xjshi <873807076@qq.com>
2019-03-13 15:07:45 +0800

    [add] 最基本的重构完成，线程池部分还需要调整下。

---
+++ libsaebase/glogTest/glogTest.cpp
@@ -0,0 +1,35 @@
+// glogTest.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+#include <iostream>
+using namespace std;
+ 
+#include <glog/logging.h>
+#pragma comment(lib, "libglog.lib")
+ 
+ int main(int argc, char **argv)
+ {
+	 FLAGS_log_dir = "E:\\";
+	 google::InitGoogleLogging("infosun");
+	 google::SetLogDestination(google::GLOG_INFO, "E:\\Logs\\INFO_");
+	 google::SetStderrLogging(google::GLOG_INFO);
+	 google::SetLogFilenameExtension("log_");
+	 FLAGS_colorlogtostderr = true;  // Set log color
+	 FLAGS_logbufsecs = 0;  // Set log output speed(s)
+	 FLAGS_max_log_size = 1024;  // Set max log file size
+	 FLAGS_stop_logging_if_full_disk = true;  // If disk is full
+	 char str[20] = "hello log!";
+	 LOG(INFO) << str;
+	 string cStr = "hello google!";
+	 LOG(INFO) << cStr;
+	 LOG(INFO) << "info test" << "hello log!";  //输出一个Info日志
+	 LOG(WARNING) << "warning test";  //输出一个Warning日志
+	 LOG(ERROR) << "error test";  //输出一个Error日志
+	 google::ShutdownGoogleLogging();
+ 
+	     system("pause");
+     return 0;
+ }
