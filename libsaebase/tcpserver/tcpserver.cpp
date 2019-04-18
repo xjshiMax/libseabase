16bc8c5520ebad459bc9100b8e9740ac442cff5e
xjshi <873807076@qq.com>
2019-04-18 10:02:30 +0800

    [modify] tcp 示例修改

--- libsaebase/tcpserver/tcpserver.cpp
+++ libsaebase/tcpserver/tcpserver.cpp
@@ -1,53 +1,52 @@
 // tcpserver.cpp : 定义控制台应用程序的入口点。
 //
-
-//#include "xEtcpserver.h"
-#include "xTcpServerBase.h"
+#include "xEtcpserver.h"
 #include "xReactorwithThread.h"
-#include "xNetdata.h"
 #include <vector>
 using namespace std;
 using namespace SEABASE;
 
-class session:public xReceiveback
-{
-public:
-	virtual void Ondata(int socketfd,char*data,int len)
-	{
-		printf("this is in session:%s\n",data);
-	}
-	virtual void Onclose(int socketfd){};
-};
+// class session:public xReceiveback
+// {
+// public:
+// 	virtual void Ondata(int socketfd,char*data,int len)
+// 	{
+// 		printf("this is in session:%s\n",data);
+// 	}
+// 	virtual void Onclose(int socketfd){};
+// };
 
 
-class tcpser :public xTcpServerBase
+class tcpser :public xtcpserver
 {
-	virtual int Onaccept(int socketfd,char*data,int len,IN xEventHandler **clientHandle=NULL)
+	virtual int Onaccept(int socketfd,char*data,int len,IN xReceivebackbase**clientHandle=NULL)
 	{
 		printf("get a accept\n");
- 		session *mysession=new session();
- 		*clientHandle=mysession;
- 		sessionset.push_back(mysession);
+//  		session *mysession=new session();
+//  		*clientHandle=mysession;
+//  		sessionset.push_back(mysession);
 		return 0;
 	}
-	virtual int Ondata(int socketfd,char*data,int len)
+	virtual void Ondata(int socketfd,char*data,int len)
 	{
 		printf("%s\n",data);
-		return 0;
+		return ;
 	}
-	virtual int Onclose(int socketfd)
+	virtual void Onclose(int socketfd)
 	{
-		return 0;
+		return ;
 	}
-	vector<session*> sessionset;
+	//vector<session*> sessionset;
 };
 int main(int argc, char* argv[])
 {
-	xReactorwithThread reactor;
+	xReactor reactor;
 	tcpser mytcp;
-	mytcp.startTCPServer(&reactor,"192.168.1.104",8000);
-	reactor.startReactorWithThread();
-	reactor.join();
+	int listenfd = mytcp.startTcpSvr("192.168.1.101",8000);
+
+	reactor.RegisterHandler(&mytcp,listenfd);
+	reactor.start();
+	//reactor.join();
 	return 0;
 }
 
